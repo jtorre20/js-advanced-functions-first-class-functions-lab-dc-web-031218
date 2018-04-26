ccd93e8172cf276e5748751ef1c765ac0b8081bb
jtorre20 <jtorres1989@gmail.com>
2018-04-26 10:51:26 -0400

    first three test

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Code your solution in this file!
+// const returnFirstTwoDrivers = function (drivers) {
+//     return drivers.slice(0,2);
+// };
+const returnFirstTwoDrivers = drivers => drivers.slice(0,2);
+
+const returnLastTwoDrivers = drivers => drivers.slice(-2);
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
